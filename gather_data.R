fba337da4543e029da1b079364a9d626a021dd3b
TIXM <alexis@TIXMs-iMac.local>
2020-06-24 16:28:10 -0500

    Initial edit

---
+++ gather_data.R
@@ -0,0 +1,6 @@
+# Locate csv
+position_data <- read_csv("csvs/positions.csv")
+#skills        <- read_csv("csvs/language_skills.csv")
+#text_blocks   <- read_csv("csvs/text_blocks.csv")
+#contact_info  <- read_csv("csvs/contact_info.csv", skip = 1)
+
