6d768d4f785a8291dcd6a53d8324baac8d267a46
TIXM <alexis@TIXMs-iMac.local>
2020-06-24 22:36:03 -0500

    Added functions and rmarkdown document

--- gather_data.R
+++ gather_data.R
@@ -1,6 +1,7 @@
 # Locate csv
 position_data <- read_csv("csvs/positions.csv")
-#skills        <- read_csv("csvs/language_skills.csv")
-#text_blocks   <- read_csv("csvs/text_blocks.csv")
-#contact_info  <- read_csv("csvs/contact_info.csv", skip = 1)
-
+skills        <- read_csv("csvs/language_skills.csv")
+text_blocks   <- read_csv("csvs/text_blocks.csv")
+contact_info  <- read_csv("csvs/contact_info.csv")
+tools <- read_csv("csvs/tools.csv")
+blog <- read.csv("csvs/blog_list.csv")
